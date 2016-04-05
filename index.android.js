9d01faca391c7cdb4631eb6edcc7140d3679f7ae
Brent Vatne <brentvatne@gmail.com>
2016-04-04 18:37:03 -0700

    Reproduced with TextInput inside of ListView

--- index.android.js
+++ index.android.js
@@ -7,8 +7,11 @@ import React, {
   AppRegistry,
   Component,
   Dimensions,
+  ScrollView,
   StyleSheet,
   Text,
+  ListView,
+  TouchableOpacity,
   View
 } from 'react-native';
 
@@ -21,14 +24,36 @@ class TextInputFocusExample extends Component {
     super(props);
 
     this.state = {
-      ex1: LOREM_IPSUM,
+      ex1: LOREM_IPSUM.slice(0, 150),
+      ex2: LOREM_IPSUM.slice(150, LOREM_IPSUM.length),
     };
   }
 
   render() {
+    let dataSource = new ListView.DataSource({rowHasChanged: () => true});
+    dataSource = dataSource.cloneWithRows([{text: 'hello'}]);
+
     return (
-      <View style={styles.container}>
-        { this._renderEx1() }
+      <View style={{flex: 1}}>
+        <Text style={styles.header}>ListView Container</Text>
+        <ListView
+          renderScrollComponent={props => <ScrollView {...props} />}
+          renderHeader={() => this._renderEx1() }
+          renderRow={() => this._renderEx2() }
+          dataSource={dataSource}
+        />
+
+        <Text style={styles.header}>ScrollView Container</Text>
+        <ScrollView>
+          {this._renderEx1()}
+          {this._renderEx2()}
+        </ScrollView>
+
+        <Text style={styles.header}>View Container</Text>
+        <View>
+          {this._renderEx1()}
+          {this._renderEx2()}
+        </View>
       </View>
     );
   }
@@ -36,9 +61,25 @@ class TextInputFocusExample extends Component {
   _renderEx1() {
     return (
       <GrowingTextInput
-        style={styles.ex1}
+        style={styles.ex}
         value={this.state.ex1}
+        maxLength={200}
+        minHeight={100}
         onChangeText={text => this.setState({ex1: text})}
+        placeholder='Placeholder text here'
+        multiline
+      />
+    );
+  }
+
+  _renderEx2() {
+    return (
+      <GrowingTextInput
+        style={styles.ex}
+        value={this.state.ex2}
+        maxLength={200}
+        minHeight={100}
+        onChangeText={text => this.setState({ex2: text})}
         placeholder='Placeholder text here'
         multiline
       />
@@ -47,19 +88,22 @@ class TextInputFocusExample extends Component {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
   ex1: {
     borderWidth: 1,
     borderColor: '#eee',
-    backgroundColor: '#fff',
-    width: Dimensions.get('window').width,
+    backgroundColor: 'transparent',
+    textAlignVertical: 'top',
+    height: 20,
+    fontSize: 18.5,
+    lineHeight: 22,
+    paddingLeft: 20,
     paddingVertical: 15,
   },
+  header: {
+    fontWeight: 'bold',
+    marginTop: 10,
+    fontSize: 15,
+  }
 });
 
 AppRegistry.registerComponent('TextInputFocusExample', () => TextInputFocusExample);
